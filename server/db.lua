b381fe6b49f3a34b168dbb50104e469cc3158e32
Lunar-Scripts <lunar.cfxresources@gmail.com>
2023-09-21 20:41:00 +0200

    Escape "stored" with backticks inside queries.

--- server/db.lua
+++ server/db.lua
@@ -1,17 +1,17 @@
 Queries = {
     getGarage = 'SELECT * FROM %s WHERE owner = ? and type = ? and job is NULL',
     getGarageSociety = 'SELECT * FROM %s WHERE job = ? and type = ?',
-    getImpound = 'SELECT * FROM %s WHERE owner = ? and type = ? and stored = 0 and job is NULL',
-    getImpoundSociety = 'SELECT * FROM %s WHERE job = ? and type = ? and stored = 0',
-    getStoredVehicle = 'SELECT * FROM %s WHERE (owner = ? or job = ?) and plate = ? and stored = ?',
-    setStoredVehicle = 'UPDATE %s SET stored = ? WHERE plate = ?',
+    getImpound = 'SELECT * FROM %s WHERE owner = ? and type = ? and `stored` = 0 and job is NULL',
+    getImpoundSociety = 'SELECT * FROM %s WHERE job = ? and type = ? and `stored` = 0',
+    getStoredVehicle = 'SELECT * FROM %s WHERE (owner = ? or job = ?) and plate = ? and `stored` = ?',
+    setStoredVehicle = 'UPDATE %s SET `stored` = ? WHERE plate = ?',
     getOwnedVehicle = 'SELECT * FROM %s WHERE (owner = ? or job = ?) and plate = ?',
     getVehicle = 'SELECT * FROM %s WHERE owner = ? and plate = ?',
     getVehicleStrict = 'SELECT * FROM %s WHERE owner = ? and plate = ? and job is NULL',
     transferVehiclePlayer = 'UPDATE %s SET owner = ? WHERE plate = ?',
     transferVehicleSociety = 'UPDATE %s SET job = ? WHERE plate = ?',
     withdrawVehicleSociety = 'UPDATE %s SET job = NULL WHERE plate = ?',
-    getStoredGarage = 'SELECT * FROM %s WHERE owner = ? and type = ? and job is NULL and stored = 1'
+    getStoredGarage = 'SELECT * FROM %s WHERE owner = ? and type = ? and job is NULL and `stored` = 1'
 }
 
 local table
